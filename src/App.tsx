9aaf6956d4d0463d15535079796d1626ecf81e2a
Mohamed Abdinasir <abdelnacerr@gmail.com>
2023-05-12 21:33:12 +1000

    binned the app bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { ThemeProvider } from "@mui/material/styles";
-import DrawerAppBar from "./features/appBar/AppBar";
 import theme from "./styles/theme";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <DrawerAppBar />;
+      <div>Hello</div>
     </ThemeProvider>
   );
 }
